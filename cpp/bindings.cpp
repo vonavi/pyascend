998e48108870062ce1c866520f70575977d23214
Vladimir Ivanov <ivvl82@gmail.com>
2025-10-14 23:31:02 +0800

    Launch the 'add_custom' kernel in Python and test its results.

--- cpp/bindings.cpp
+++ cpp/bindings.cpp
@@ -6,6 +6,7 @@ namespace py = pybind11;
 PYBIND11_MODULE(_ascend, m) {
   m.doc() = "Pybind11-powered runtime for launching kernels on Ascend NPU.";
 
-  m.def("kernel_launch", &kernel_launch, py::arg("name"),
-        "Launch a kernel by name.");
+  m.def("kernel_launch", &kernelLaunch, py::arg("kernel"), py::pos_only(),
+        py::arg("objpath"), py::arg("datadir"),
+        "Launch a kernel on Ascend NPU.");
 }
