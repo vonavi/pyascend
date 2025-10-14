90e1866489daec2a8f7c7dbb776885a9d17e0a47
Vladimir Ivanov <ivvl82@gmail.com>
2025-10-15 00:06:43 +0800

    Move C++ definitions to the header file.

--- include/ascend.hpp
+++ include/ascend.hpp
@@ -3,6 +3,34 @@
 
 #include <string>
 
+typedef int16_t float16_t;
+
+#define MAX_BIN_LENGTH 0x1000000
+
+// ---- Error checking helpers ----
+
+#define CHECK_ACL(x)                                                           \
+  do {                                                                         \
+    aclError __ret = x;                                                        \
+    if (ACL_ERROR_NONE != __ret) {                                             \
+      std::ostringstream oss;                                                  \
+      oss << __FILE__ << ":" << __LINE__ << " aclError:" << __ret;             \
+      throw std::runtime_error(oss.str());                                     \
+    }                                                                          \
+  } while (0)
+
+#define CHECK_RT(x)                                                            \
+  do {                                                                         \
+    rtError_t __ret = x;                                                       \
+    if (RT_ERROR_NONE != __ret) {                                              \
+      std::ostringstream oss;                                                  \
+      oss << __FILE__ << ":" << __LINE__ << " rtError:" << __ret;              \
+      throw std::runtime_error(oss.str());                                     \
+    }                                                                          \
+  } while (0)
+
+// ---- Main functions ----
+
 void kernelLaunch(const std::string &kernel, const std::string &objPath,
                   const std::string &dataDir);
 
