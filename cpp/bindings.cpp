4623e16b720feae74fa824d2064ab98351bf4cda
Vladimir Ivanov <ivvl82@gmail.com>
2025-10-16 19:09:53 +0800

    Implement Buffer protocol for Python's GMem class.

--- cpp/bindings.cpp
+++ cpp/bindings.cpp
@@ -10,16 +10,33 @@ PYBIND11_MODULE(_ascend, m) {
 
   ascendInitialize();
 
-  py::class_<GMem>(m, "GMem").def(py::init([](const py::buffer &buffer) {
-    py::buffer_info info = buffer.request();
-    if (info.format[0] != py::dtype("float16").char_())
-      throw py::type_error("Input must have dtype float16");
-    if (info.ndim != 1)
-      throw py::value_error("Input must be 1-D vector");
+  py::class_<GMem>(m, "GMem", py::buffer_protocol())
+      .def(py::init([](const py::buffer &buffer) {
+        py::buffer_info info = buffer.request();
+        if (info.format[0] != py::dtype("float16").char_())
+          throw py::type_error("Input must have dtype float16");
+        if (info.ndim != 1)
+          throw py::value_error("Input must be 1-D vector");
 
-    size_t nbytes = info.shape[0] * info.itemsize;
-    return new GMem(info.ptr, nbytes);
-  }));
+        size_t nbytes = info.shape[0] * info.itemsize;
+        return new GMem(info.ptr, nbytes);
+      }))
+      .def_buffer([](const GMem &gm) {
+        char *data = new char[gm.nbytes()];
+        gm.copyTo(data);
+
+        py::dtype dtype = py::dtype("float16");
+        size_t size = gm.nbytes() / dtype.itemsize();
+        std::string format(1, dtype.char_());
+        return py::buffer_info(
+            data,              /* Pointer to buffer */
+            dtype.itemsize(),  /* Size of one scalar */
+            format,            /* Python struct-style format descriptor */
+            1,                 /* Number of dimensions */
+            {size},            /* Buffer dimensions */
+            {dtype.itemsize()} /* Strides (in bytes) for each index */
+        );
+      });
 
   m.def(
       "kernel_launch",
@@ -31,12 +48,7 @@ PYBIND11_MODULE(_ascend, m) {
         size_t nbytes = gmX.nbytes();
         GMem gmZ(nbytes);
         kernelLaunch(kernel, gmX, gmY, gmZ, objPath);
-        void *outDataZ = new char[nbytes];
-        gmZ.copyTo(outDataZ);
-
-        py::dtype dtype = py::dtype("float16");
-        size_t size = nbytes / dtype.itemsize();
-        return py::array(dtype, {size}, {dtype.itemsize()}, outDataZ);
+        return gmZ;
       },
       py::arg("kernel"), py::arg("x"), py::arg("y"), py::pos_only(),
       py::arg("objpath"), "Launch a kernel on Ascend NPU.");
